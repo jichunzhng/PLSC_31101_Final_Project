bf6efe8ae780473f88be2ce3dc4bb7be658ebff1
jichunzhng <75768361+jichunzhng@users.noreply.github.com>
2020-12-11 06:37:09 +0800

    Add files via upload

---
+++ Code/04 Data_Cleasing_Military_GDP.R
@@ -0,0 +1,37 @@
+##This script performs data cleasing for the military_GDP graph
+
+#load packages
+require(readxl)
+require(tidyverse)
+
+#load the raw dataset
+military_GDP <- read_excel("data/SIPRI_Data.xlsx",
+                           sheet="Share of GDP")
+#delete the first 4 rows
+military_GDP <- military_GDP[-c(1:4),]
+
+#delete inappropriate column names
+colnames(military_GDP) <- as.character(unlist(military_GDP[1,]))
+military_GDP <- military_GDP[-1,]
+
+# get useful columns and rows 
+military_GDP <- military_GDP %>% 
+  select(-Notes) %>% 
+  gather("year","Military_GDP",c(-Country)) %>% 
+  filter(Country=='China'|
+           Country=='USA'|
+           Country=='Japan'|
+           Country=='Taiwan'|
+           Country=='Korea, South'
+  )
+
+# perform basic calculation to improve presentation
+military_GDP$Military_GDP <- as.numeric(military_GDP$Military_GDP)*100 
+military_GDP$year <- as.integer(military_GDP$year)
+#change the class of `year` to integer 
+military_GDP <- military_GDP %>% 
+  filter(!is.na(Military_GDP))
+#remove NA values
+
+write.csv(military_GDP, "military_GDP.csv")
+
